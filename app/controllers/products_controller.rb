f3562bacc89e1d41d652f9ee2ba1b65727cdb437
Carl Abelin <carl@capp.se>
2016-01-29 10:13:55 +0100

    New and update product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,4 +8,35 @@ class ProductsController < ApplicationController
 		@product = Product.find(params[:id])
 	end
 
+	def edit
+		@product = Product.find(params[:id])
+	end
+
+	def update
+		@product = Product.find(params[:id])
+		@product.update(product_params)
+		redirect_to @product
+	end
+
+	def new
+		@product = Product.new
+	end
+
+	def create
+
+		@product = Product.new(product_params)
+		@product.save
+		redirect_to @product
+	end
+
+private
+
+	def product_params
+		params.require(:product).
+										permit(:name, :description, :price, :stock, :image)
+
+	end
+
+
+
 end
