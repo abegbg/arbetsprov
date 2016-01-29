371a0caafebf2750c5bf6e45ad10a4f829c987de
Carl Abelin <carl@capp.se>
2016-01-29 08:36:39 +0100

    Stock helper

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,13 @@
 module ProductsHelper
+
+	def format_stock(product)
+		if product.no_stock?
+			content_tag(:p, "Slut i lager!", class: "low_stock")
+		elsif product.stock_low?
+			content_tag(:p, product.stock.to_s + " st i lager", class: "low_stock")
+		else
+			content_tag(:p, product.stock.to_s + " st i lager", class: "normal_stock")
+		end
+	end
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,11 @@
 class Product < ActiveRecord::Base
 
-	
+	def stock_low?
+		stock < 10
+	end	
+
+	def no_stock?
+		stock == 0
+	end
 
 end
