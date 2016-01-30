c86d62dc95a77422767d979421cb7435596fbab8
Carl Abelin <carl@capp.se>
2016-01-30 12:44:39 +0100

    Validations product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,13 @@
 class Product < ActiveRecord::Base
 
+
+	validates :name, presence: true	
+	validates :price, numericality: { greater_than_or_equal_to: 0 }
+	validates :description, length: { minimum: 25 }
+	validates :stock, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+
+
 	def stock_low?
 		stock < 10
 	end	
